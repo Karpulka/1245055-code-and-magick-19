a63e0882cbff9f055f9b9c5511afbdc7d3138ba5
Alyona Karpova <karpova.dev@yandex.ru>
2020-01-23 21:39:48 +0300

    добавлена переменная, хранящая в себе параметры для отрисовки облака (методы и координаты)

--- js/stat.js
+++ js/stat.js
@@ -17,3 +17,92 @@ var STATISTIC_MARGIN_X = 50;
 var STATISTIC_START_X = 150;
 var STATISTIC_START_Y = 100;
 var LETTER_WIDTH = 10;
+
+var cloudParams = [
+  {
+    method: 'arc',
+    params: [
+      170,
+      150,
+      60,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'arc',
+    params: [
+      250,
+      100,
+      80,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'arc',
+    params: [
+      495,
+      130,
+      30,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'ellipse',
+    params: [
+      340,
+      150,
+      190,
+      100,
+      0,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'arc',
+    params: [
+      425,
+      110,
+      80,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'ellipse',
+    params: [
+      390,
+      210,
+      140,
+      80,
+      0,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'arc',
+    params: [
+      205,
+      170,
+      80,
+      0,
+      CIRCUMFERENCE
+    ]
+  },
+  {
+    method: 'ellipse',
+    params: [
+      230,
+      230,
+      120,
+      60,
+      0,
+      0,
+      CIRCUMFERENCE
+    ]
+  }
+];
