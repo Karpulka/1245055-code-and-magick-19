33e6475482f50c79a360d568ee812ac2e62479e9
Alyona Karpova <karpova.dev@yandex.ru>
2020-01-26 23:15:26 +0300

    доработан рендеринг фигур (облако и прямоугольники) и текста

--- js/stat.js
+++ js/stat.js
@@ -14,6 +14,29 @@ var STATISTIC_START_X = 150;
 var STATISTIC_START_Y = 100;
 var LETTER_WIDTH = 10;
 var MY_STATISTIC_COLOR = 'rgba(255, 0, 0, 1)';
+var CLOUD_SHAPE = [
+  ['arc', 160, 140, 60],
+  ['arc', 240, 90, 80],
+  ['arc', 485, 120, 30],
+  ['ellipse', 330, 140, 190, 100],
+  ['arc', 415, 100, 80],
+  ['ellipse', 380, 200, 140, 80],
+  ['arc', 195, 160, 80],
+  ['ellipse', 220, 220, 120, 60]
+];
+var TOP_TEXTS = [
+  {
+    text: 'Ура вы победили!',
+    x: 175,
+    y: 47
+  },
+  {
+    text: 'Список результатов:',
+    x: 185,
+    y: 65
+  }
+];
+var MY_NAME_FOR_STATISTIC = 'Вы';
 
 function Coord(x, y) {
   return {x: x, y: y};
@@ -25,27 +48,19 @@ var ctxCallBuilder = {
   },
   'ellipse': function (ctx, center, radiusX, radiusY) {
     ctx.ellipse(center.x, center.y, radiusX, radiusY, 0, 0, CIRCUMFERENCE);
+  },
+  'fillRect': function (ctx, coordinates, width, height) {
+    ctx.fillRect(coordinates.x, coordinates.y, width, height);
   }
 };
 
-var CLOUD_SHAPE = [
-  ['arc', 160, 140, 60],
-  ['arc', 240, 90, 80],
-  ['arc', 485, 120, 30],
-  ['ellipse', 330, 140, 190, 100],
-  ['arc', 415, 100, 80],
-  ['ellipse', 380, 200, 140, 80],
-  ['arc', 195, 160, 80],
-  ['ellipse', 220, 220, 120, 60]
-];
-
-var renderCloud = function (ctx, color, shift) {
+var renderFigure = function (ctx, params, color, shift) {
   if (!shift) {
     shift = new Coord(0, 0);
   }
   ctx.fillStyle = color;
   ctx.beginPath();
-  CLOUD_SHAPE.forEach(function (args) {
+  params.forEach(function (args) {
     var center = new Coord(args[1] + shift.x, args[2] + shift.y);
     ctxCallBuilder[args[0]](ctx, center, args[3], args[4]);
   });
@@ -53,9 +68,13 @@ var renderCloud = function (ctx, color, shift) {
   ctx.fill();
 };
 
-var renderRectangle = function (ctx, x, y, width, height, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
+var getRectColor = function (name) {
+  var color = MY_STATISTIC_COLOR;
+  if (name !== MY_NAME_FOR_STATISTIC) {
+    var saturation = Math.ceil(Math.random(0, 100) * 100);
+    color = 'hsl(240deg, ' + saturation + '%, 50%)';
+  }
+  return color;
 };
 
 var getTextStartX = function (startX, blockWidth, letterWidth, textLength) {
@@ -63,23 +82,22 @@ var getTextStartX = function (startX, blockWidth, letterWidth, textLength) {
   return textStartX;
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, SHADOW_COLOR, new Coord(10, 10));
-  renderCloud(ctx, CLOUD_COLOR, new Coord(0, 0));
-
+var renderText = function (ctx, text, coordinates) {
   ctx.fillStyle = TEXT_COLOR;
   ctx.font = TEXT_PARAMS;
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', 175, 47);
-  ctx.fillText('Список результатов:', 185, 65);
+  ctx.fillText(text, coordinates.x, coordinates.y);
+};
 
-  var maxTime = 0;
+window.renderStatistics = function (ctx, names, times) {
+  renderFigure(ctx, CLOUD_SHAPE, SHADOW_COLOR, new Coord(10, 10));
+  renderFigure(ctx, CLOUD_SHAPE, CLOUD_COLOR, new Coord(0, 0));
 
-  // Math.max.apply(null, times)
-  // Math.max(...times)
-  times.forEach(function (value) {
-    maxTime = maxTime < value ? value : maxTime;
+  TOP_TEXTS.forEach(function (item) {
+    renderText(ctx, item.text, new Coord(item.x, item.y));
   });
+
+  var maxTime = Math.max.apply(null, times);
 
   var marginLeft = STATISTIC_MARGIN_X + STATISTIC_WIDTH;
   var startX = STATISTIC_START_X;
@@ -88,28 +106,18 @@ window.renderStatistics = function (ctx, names, times) {
   for (var l = 0; l < times.length; l++, startX += marginLeft) {
     var height = Math.ceil(STATISTIC_HEIGHT * times[l] / maxTime);
     var startY = STATISTIC_START_Y + STATISTIC_HEIGHT - height;
-
-    // Напрашивается функция getRectColor
-    var color = MY_STATISTIC_COLOR;
-
-    if (names[l] !== 'Вы') {
-      var saturation = Math.ceil(Math.random(0, 100) * 100);
-      color = 'hsl(240deg, ' + saturation + '%, 50%)';
-    }
-
-    renderRectangle(ctx, startX, startY, STATISTIC_WIDTH, height, color);
+    var color = getRectColor(names[l]);
+    var rectangle = [['fillRect', startX, startY, STATISTIC_WIDTH, height]];
+    renderFigure(ctx, rectangle, color);
 
     var nameLength = names[l].length;
     var nameStartX = getTextStartX(startX, STATISTIC_WIDTH, LETTER_WIDTH, nameLength);
-
-    ctx.fillStyle = TEXT_COLOR; // Строка 67 дублирует то же самое, напрашивается функция renderText
-
-    ctx.fillText(names[l], nameStartX, nameStartY);
+    renderText(ctx, names[l], new Coord(nameStartX, nameStartY));
 
     var time = Math.ceil(times[l]).toString();
     var timeLength = time.length;
     var timeStartX = getTextStartX(startX, STATISTIC_WIDTH, LETTER_WIDTH, timeLength);
     var timeStartY = startY - TIME_TEXT_TOP_MARGIN;
-    ctx.fillText(time, timeStartX, timeStartY);
+    renderText(ctx, time, new Coord(timeStartX, timeStartY));
   }
 };
