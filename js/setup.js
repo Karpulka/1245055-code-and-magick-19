14d62df09b550d8fd5b807f0fdddb7ba632698c1
Alyona Karpova <karpova.dev@yandex.ru>
2020-02-03 12:08:33 +0300

    добавлено открытие / закрытие окна настроек

--- js/setup.js
+++ js/setup.js
@@ -5,10 +5,14 @@ var LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вал�
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
+var ENTER_KEY = 'Enter';
+var ESC_KEY = 'Escape';
 
 var setupBlock = document.querySelector('.setup');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 var similarListElement = setupBlock.querySelector('.setup-similar-list');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setupBlock.querySelector('.setup-close');
 
 var getRandomItemFromArray = function (array) {
   var min = 0;
@@ -40,7 +44,6 @@ var renderWizard = function (wizard) {
 };
 
 if (setupBlock) {
-  setupBlock.classList.remove('hidden');
   var wizards = getOtherWizards(WIZARDS_COUNT);
   var fragment = document.createDocumentFragment();
   wizards.forEach(function (wizard) {
@@ -49,3 +52,39 @@ if (setupBlock) {
   similarListElement.appendChild(fragment);
   setupBlock.querySelector('.setup-similar').classList.remove('hidden');
 }
+
+var openSetupBlock = function () {
+  setupBlock.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscPressHandler);
+};
+
+var closePopup = function () {
+  setupBlock.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscPressHandler);
+};
+
+var popupEscPressHandler = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+setupOpen.addEventListener('click', function () {
+  openSetupBlock();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openSetupBlock();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
