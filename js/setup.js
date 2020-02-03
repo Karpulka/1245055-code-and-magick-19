23569ae88b8505ab901f34120b9a2322137590ea
Alyona Karpova <karpova.dev@yandex.ru>
2020-02-03 20:39:14 +0300

    добавлены обработчики событий для изменения цвета плаща, глаз, фаербола

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Крис�
 var LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_COUNT = 4;
 var ENTER_KEY = 'Enter';
 var ESC_KEY = 'Escape';
@@ -13,6 +14,10 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 var similarListElement = setupBlock.querySelector('.setup-similar-list');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setupBlock.querySelector('.setup-close');
+var setupWizard = setupBlock.querySelector('.setup-wizard');
+var setupWizardCoat = setupWizard.querySelector('.wizard-coat');
+var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
+var setupWizardFireball = setupBlock.querySelector('.setup-fireball-wrap');
 
 var getRandomItemFromArray = function (array) {
   var min = 0;
@@ -43,6 +48,15 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+var getNextArrayElement = function (key, array) {
+  return key === array.length - 1 ? array[0] : array[key + 1];
+};
+
+var getWizardElementColor = function (color, colors) {
+  var colorKey = colors.indexOf(color);
+  return getNextArrayElement(colorKey, colors);
+};
+
 if (setupBlock) {
   var wizards = getOtherWizards(WIZARDS_COUNT);
   var fragment = document.createDocumentFragment();
@@ -56,6 +70,15 @@ if (setupBlock) {
 var openSetupBlock = function () {
   setupBlock.classList.remove('hidden');
   document.addEventListener('keydown', popupEscPressHandler);
+  setupWizardCoat.addEventListener('click', function (evt) {
+    evt.target.style.fill = getWizardElementColor(evt.target.style.fill, COAT_COLORS);
+  });
+  setupWizardEyes.addEventListener('click', function (evt) {
+    evt.target.style.fill = getWizardElementColor(evt.target.style.fill, EYES_COLORS);
+  });
+  setupWizardFireball.addEventListener('click', function (evt) {
+    evt.currentTarget.style.background = getWizardElementColor(evt.currentTarget.style.background, FIREBALL_COLORS);
+  });
 };
 
 var closePopup = function () {
